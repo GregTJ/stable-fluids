cff74663648f4da1ec32a3a09f1b783a08fba7ed
gregtj <gregorytj@outlook.com>
2021-04-15 13:01:12 -0500

    Removed unnecessary separation of variables.

--- numerical.py
+++ numerical.py
@@ -14,7 +14,7 @@ def difference(derivative, accuracy=1):
     return result[-derivative - 1] * factorial(derivative)
 
 
-def operator(shape, *differences, separate=False):
+def operator(shape, *differences):
     def factors():
         for d, coefficients in zip(shape, differences):
             middle = len(coefficients) // 2
@@ -26,22 +26,8 @@ def operator(shape, *differences, separate=False):
     if len(differences) == 1:
         differences *= len(shape)
 
-    if separate:
-        ids = tuple(sp.identity(d) for d in shape)
-        factors = (ids[:i] + (f,) + ids[i + 1:] for i, f in enumerate(factors()))
-        _ = partial(sp.kron, format='csc')
-
-        # The sum of these kron product folds is
-        # equivalent to the kronsum of the factors.
-        # This identity can be derived from the
-        # properties of the kronecker product and
-        # is analogous to separation of variables.
-        # See: https://en.wikipedia.org/wiki/Kronecker_sum_of_discrete_Laplacians
-        return tuple(reduce(_, f) for f in factors)
-
-    else:
-        # Credit to Philip Zucker for figuring out
-        # that kronsum's argument order is reversed.
-        # Without that bit of wisdom I'd have lost it.
-        _ = partial(sp.kronsum, format='csc')
-        return reduce(lambda a, f: _(f, a), factors())
+    # Credit to Philip Zucker for figuring out
+    # that kronsum's argument order is reversed.
+    # Without that bit of wisdom I'd have lost it.
+    _ = partial(sp.kronsum, format='csc')
+    return reduce(lambda a, f: _(f, a), factors())
