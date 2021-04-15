721d59748010b9ab49e1d442065c3ffe84ff2e1b
gregtj <gregorytj@outlook.com>
2021-04-15 13:16:23 -0500

    Simplification of operator matrix generation.

--- numerical.py
+++ numerical.py
@@ -1,4 +1,5 @@
-from functools import reduce, partial
+from functools import reduce
+from itertools import cycle
 from math import factorial
 
 import numpy as np
@@ -10,24 +11,13 @@ def difference(derivative, accuracy=1):
     # http://web.media.mit.edu/~crtaylor/calculator.html
     radius = accuracy + (derivative + 1) // 2 - 1
     points = range(-radius, radius + 1)
-    result = np.linalg.inv(np.vander(points))
-    return result[-derivative - 1] * factorial(derivative)
+    coefficients = np.linalg.inv(np.vander(points))
+    return coefficients[-derivative - 1] * factorial(derivative), points
 
 
 def operator(shape, *differences):
-    def factors():
-        for d, coefficients in zip(shape, differences):
-            middle = len(coefficients) // 2
-            indices = range(-middle, middle + 1)
-            diagonals = zip(indices, coefficients)
-            diagonals = (c * np.ones(d - abs(i)) for i, c in diagonals)
-            yield sp.diags(diagonals, indices)
-
-    if len(differences) == 1:
-        differences *= len(shape)
-
     # Credit to Philip Zucker for figuring out
     # that kronsum's argument order is reversed.
     # Without that bit of wisdom I'd have lost it.
-    _ = partial(sp.kronsum, format='csc')
-    return reduce(lambda a, f: _(f, a), factors())
+    factors = (sp.diags(*diff, shape=(dim,) * 2) for dim, diff in zip(shape, cycle(differences)))
+    return reduce(lambda a, f: sp.kronsum(f, a, format='csc'), factors)
